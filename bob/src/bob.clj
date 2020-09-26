ed8571f736d4e17fa5aa201c2d247904f7020a84
Leonam Pereira Dias <leonam.pd@gmail.com>
2020-09-26 20:42:48 -0300

    add solution for Bob exercise

---
+++ bob/src/bob.clj
@@ -0,0 +1,31 @@
+(ns bob
+  (:require [clojure.string :as string]))
+
+(defn question?
+  [input]
+  (string/ends-with? input "?"))
+
+(defn silence?
+  [input]
+  (string/blank? input))
+
+(defn yelling-me?
+  [input]
+  (and
+    (not= (string/upper-case input)
+         (string/lower-case input))
+    (= (string/upper-case input) input)))
+
+(defn yelling-question?
+  [input]
+  (and (yelling-me? input) (question? input)))
+
+(defn response-for
+  [input]
+  (let [trimedInput (string/trim input)]
+    (cond
+      (silence? trimedInput) "Fine. Be that way!"
+      (yelling-question? trimedInput) "Calm down, I know what I'm doing!"
+      (yelling-me? trimedInput) "Whoa, chill out!"
+      (question? trimedInput) "Sure."
+      :else "Whatever.")))
